e1e54700e81dc94d0f9e19ddcc92b2e1addd7628
kouamewily <alltechnology.ci@gmail.com>
2022-04-21 23:37:41 +0200

    ajout host

--- ecole/settings.py
+++ ecole/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%djk1h4qhor)#fzb)swn^!!6)nfd#&h8x)76w4n!#gk!j@0d_#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://allschoolapp.herokuapp.com/']
 
 
 # Application definition
